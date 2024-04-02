27f3c1c177e21d5b629033d69e269abd5f3b0803
wnccys <ipooo346@gmail.com>
2024-04-02 16:43:38 -0300

    Feat: fix variable not showing at template

--- lib/erbr.rb
+++ lib/erbr.rb
@@ -4,8 +4,10 @@ module ERBR
     def self.render(template_name, layout_name, *args)
         template_file = File.read(template_name)
         layout_file = File.read(layout_name)
+        @args = args
 
         rendered_template = ERB.new(template_file).result(binding)
+        print rendered_template
 
         print bind_template_to_layout(layout_file) { rendered_template }
     end
@@ -17,4 +19,4 @@ module ERBR
     end
 end
 
-ERBR::render(__dir__ + "/file2.html.erb", __dir__ + "/file1.html.erb")
+ERBR::render(__dir__ + "/file2.html.erb", __dir__ + "/file1.html.erb", @pokes=["charmander", "squirtle", "bulbasaur"])
