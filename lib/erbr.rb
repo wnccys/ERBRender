d57593231435c38a8bc6bd6adf6701ba55042c1d
wnccys <ipooo346@gmail.com>
2024-04-02 15:24:48 -0300

    Feat: erbr

---
+++ lib/erbr.rb
@@ -0,0 +1,20 @@
+require 'erb'
+
+module ERBR
+    def self.render(template_name, layout_name, *args)
+        template_file = File.read(template_name)
+        layout_file = File.read(layout_name)
+
+        rendered_template = ERB.new(template_file).result(binding)
+
+        print bind_template_to_layout(layout_file) { rendered_template }
+    end
+
+    def self.bind_template_to_layout(layout_file)
+        yield
+
+        ERB.new(layout_file).result(binding)
+    end
+end
+
+ERBR::render(__dir__ + "/file2.html.erb", __dir__ + "/file1.html.erb")
