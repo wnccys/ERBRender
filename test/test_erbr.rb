ed9236238b10ba446ceeca6baefb8d2730649fcc
wnccys <ipooo346@gmail.com>
2024-04-23 23:18:52 -0300

    removes test folder (inits RSpec installation)

--- test/test_erbr.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-require 'erbr'
-
-class ERBRTest < Minitest::Test
-    def base_test
-        assert_equal "Successfully binded.", 
-            ERBR::render(__dir__ + "../lib/template.html.erb", __dir__ + "../lib/layout.html.erb", [:first => "1"])
-    end
-end
