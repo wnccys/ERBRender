e06a8d831d0bb4b71e6ac9f4152afa70605da816
wnccys <ipooo346@gmail.com>
2024-04-23 23:27:41 -0300

    adds RSpec test file properly

---
+++ spec/rspec_test_erbr.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+require 'erb'
+require_relative '../lib/erbr'
+
+RSpec.describe ERBR do 
+    describe '.render' do 
+        context 'when rendering a template' do 
+            it 'renders the content inside template file' do
+                result = ERBR.render(__dir__ + '/../lib/template.html.erb', nil) 
+                expect(result).to include ('<p>Template Content</p>')
+            end
+        end
+
+        context 'when rendering with layout' do
+            it 'renders template and binds it to layout' do 
+                result = ERBR.render(__dir__ + '/../lib/template.html.erb', __dir__ + '/../lib/layout.html.erb')
+                expect(result).to include ('<title>Layout it working!</title>
+<p>Template Content</p>')
+            end
+        end
+
+        context 'when rendering with layout and arguments' do
+            it 'renders template and binds it to layout with arguments assigned' do 
+                result = ERBR.render(__dir__ + '/../lib/template.html.erb',
+                  __dir__ + '/../lib/layout.html.erb', :poke => "charmander")
+                expect(result).to include ('<title>Layout it working!</title>
+<p>Template Content</p>
+
+<p>charmander</p>') 
+            end
+        end
+    end
+end
