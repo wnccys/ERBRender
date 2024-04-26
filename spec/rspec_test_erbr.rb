9bbb15cb0818e7cf32eeac4d7b4395c3a268623d
wnccys <ipooo346@gmail.com>
2024-04-26 12:14:23 -0300

    fixes test example results

--- spec/rspec_test_erbr.rb
+++ spec/rspec_test_erbr.rb
@@ -7,26 +7,26 @@ RSpec.describe ERBR do
         context 'when rendering a template' do 
             it 'renders the content inside template file' do
                 result = ERBR.render(__dir__ + '/../lib/template.html.erb', nil) 
-                expect(result).to include ('<p>Template Content</p>')
+                expect(result).to include ('<p>Template Content Binded With Layout</p>')
             end
         end
 
         context 'when rendering with layout' do
             it 'renders template and binds it to layout' do 
                 result = ERBR.render(__dir__ + '/../lib/template.html.erb', __dir__ + '/../lib/layout.html.erb')
-                expect(result).to include ('<title>Layout it working!</title>
-<p>Template Content</p>')
+                expect(result).to include ('<title>Layout Title</title>
+<p>Template Content Binded With Layout</p>')
             end
         end
 
         context 'when rendering with layout and arguments' do
             it 'renders template and binds it to layout with arguments assigned' do 
                 result = ERBR.render(__dir__ + '/../lib/template.html.erb',
-                  __dir__ + '/../lib/layout.html.erb', :poke => "charmander")
-                expect(result).to include ('<title>Layout it working!</title>
-<p>Template Content</p>
+                  __dir__ + '/../lib/layout.html.erb', :msg => "Binded Argument!")
+                expect(result).to include ('<title>Layout Title</title>
+<p>Template Content Binded With Layout</p>
 
-<p>charmander</p>') 
+<span>Binded Argument!</span>') 
             end
         end
     end
